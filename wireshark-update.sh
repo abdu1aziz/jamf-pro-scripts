6d93defc866a9f80f699b60c7cfc95c400a61142
Abdul Aziz <26386672+abdu1aziz@users.noreply.github.com>
2023-05-31 17:24:33 -0400

    Create wireshark-update.sh
    Added Wireshark script.

---
+++ wireshark-update.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Check the macOS device architecture
+arch_name=$(uname -m)
+
+if [[ "$arch_name" == "arm64" ]]; then
+    # Apple Silicon architecture
+    download_url=$(curl -s https://www.wireshark.org/download.html | grep -Eo "https:\/\/[^\"]+_macOS_arm64\.dmg" | head -n 1)
+elif [[ "$arch_name" == "x86_64" ]]; then
+    # Intel architecture
+    download_url=$(curl -s https://www.wireshark.org/download.html | grep -Eo "https:\/\/[^\"]+_macOS\.dmg" | head -n 1)
+else
+    echo "Unsupported architecture: $arch_name"
+    exit 1
+fi
+
+if [[ -z "$download_url" ]]; then
+    echo "Failed to retrieve download URL."
+    exit 1
+fi
+
+# Download the appropriate version
+echo "Downloading the appropriate version for $arch_name..."
+curl -O "$download_url"
+
+# Mount the DMG file
+echo "Mounting the DMG file..."
+dmg_file=$(basename "$download_url")
+hdiutil attach "$dmg_file"
+
+# Find the Wireshark application in the mounted DMG
+echo "Finding the Wireshark application..."
+app_path=$(find /Volumes -iname "Wireshark*.app" -maxdepth 1 -type d)
+
+if [[ -z "$app_path" ]]; then
+    echo "Failed to find Wireshark application in the DMG."
+    exit 1
+fi
+
+# Copy the Wireshark application to the Applications folder
+echo "Installing Wireshark..."
+cp -R "$app_path" /Applications/
+
+# Unmount the DMG file
+echo "Unmounting the DMG file..."
+hdiutil detach "/Volumes/Wireshark" >/dev/null
+
+# Cleanup the downloaded files
+echo "Cleaning up..."
+rm -f "$dmg_file"
+
+echo "Wireshark installation complete."
